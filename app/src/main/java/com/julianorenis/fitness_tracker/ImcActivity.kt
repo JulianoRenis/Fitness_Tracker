31660de767ff44d05bff2effd8ae2af9f56ee81a
JulianoBaier <julianorenis@gmail.com>
2022-12-14 12:50:28 -0300

    Adicionando Style nos componentes da interface - TextView - EditText - Buttom

---
+++ app/src/main/java/com/julianorenis/fitness_tracker/ImcActivity.kt
@@ -0,0 +1,11 @@
+package com.julianorenis.fitness_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
--- app/src/main/java/com/julianorenis/fitness_tracker/MainActivity.kt
+++ app/src/main/java/com/julianorenis/fitness_tracker/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.julianorenis.fitness_tracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnIMC: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnIMC = findViewById(R.id.btn_imc)
+
+        btnIMC.setOnClickListener{
+            var i = Intent(this, ImcActivity::class.java)
+            startActivity(i)
+
+        }
+
     }
 }
